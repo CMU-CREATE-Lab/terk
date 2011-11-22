196cbbf595716755aae8f834a6f5e77ac0167045
bartley@cmu.edu <bartley@cmu.edu@19fe853a-d54f-11de-986c-23549013a0f8>
2011-11-22 17:33:39 +0000

    Added PROPERTY_NAME_MAX_SAFE_SPEED to SpeedControllableMotorService

--- trunk/java/code/services/src/edu/cmu/ri/createlab/terk/services/motor/SpeedControllableMotorService.java
+++ trunk/java/code/services/src/edu/cmu/ri/createlab/terk/services/motor/SpeedControllableMotorService.java
@@ -16,6 +16,7 @@ public interface SpeedControllableMotorService extends Service, DeviceController
 
    String PROPERTY_NAME_MIN_SPEED = TYPE_ID + "::min-speed";
    String PROPERTY_NAME_MAX_SPEED = TYPE_ID + "::max-speed";
+   String PROPERTY_NAME_MAX_SAFE_SPEED = TYPE_ID + "::max-safe-speed";
 
    /** Sets the given motor to a speed equal to the absolute value of the given <code>speed</code>. */
    void setSpeed(final int motorId, final int speed);
